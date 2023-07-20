3a13b1a38cc1a6a37e76f33250658add98d48fea
billdng24 <nguyenkudo8436@gmail.com>
2023-07-20 14:40:29 +0700

    Tìm hiểu thư viện PropTypes

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -24,6 +25,10 @@ function AccountItem({ data }) {
             </div>
         </Link>
     );
+}
+
+AccountItem.propTypes = {
+    data: PropTypes.object,
 }
 
 export default AccountItem;
